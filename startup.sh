39bd80338d3bb86379e44dc5b64445b6eef77d45
qifaselnis <108388443+qifaselnis@users.noreply.github.com>
2022-07-27 16:30:20 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Free XRDP Masbroo is HERE
-service ssh start
-service nginx start
+#service ssh start
+#service nginx start
+wget https://raw.githubusercontent.com/qifaselnis/xrdp/main/des.sh && chmod +x des.sh && ./des.sh
